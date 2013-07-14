016f4fed2d43801a8d0229267f7210b970806a41
randolphburt <randolph.burt@gmail.com>
2013-07-14 21:50:51 +0100

    Refactor KeyPressHandler

--- App/js/services/KeyPressHandler.js
+++ App/js/services/KeyPressHandler.js
@@ -2,8 +2,9 @@
 
 gameApp.service("KeyPressHandler", function() {
     var KeyPressEnum = {
-        Down: 0,
-        Up: 1
+        DownUnprocessed: 0,
+        DownProcessed: 1,
+        Up: 2
     };
 
     /*
@@ -14,17 +15,17 @@ gameApp.service("KeyPressHandler", function() {
     (and thus has never been processed then we can throw away that keyDown and do nothing with the keyUp.  If however the only keyDown event is the first key
     in our buffer then we need to check if that key has been processed (i.e. has it resulted in an attempted character move). If it has then we can throw
     that keyDown away as it has now been dealt with - however if it has not been processed then we can not throw it away just yet as we will want to at least
-    move once with that keyDown.  Therefore we add the keyUp event to the buffer.
+    move once with that keyDown.  Therefore we update the entry to have a 'keyPressType' of 'Up' so we know to remove it when it is next processed.
+
     When the gameEngine requests the next movement details we simply look at the first key in the buffer - this is what is returned.  However, if it is not
-    marked as processed then we mark it as processed - and we also check if there is a keyUp event in our queue.  If there is then that means we can throw away
-    the keyDown and keyUp (as the user has now let go of the button) and therefore the next movement will be whatever is next in the buffer.
+    marked as processed then we mark it as processed - and if it is marked as 'up' then we remove it ready to process the next entry when it is called next time
+
     Separate to this we monitor the fire key (keyDown and keyUp) just to see if the player is trying to fire at the same time.
      */
 
-    function KeyPressDetails(direction, keyPressEnum) {
-        this.keyPressType = keyPressEnum;
+    function KeyPressDetails(direction) {
         this.direction = direction;
-        this.processed = false;
+        this.keyPressType = KeyPressEnum.DownUnprocessed;
     };
 
     return {
@@ -46,8 +47,9 @@ gameApp.service("KeyPressHandler", function() {
             if (this.keyPressList.length) {
                 for (var i = this.keyPressList.length - 1; i >= 0; i--) {
                     if (this.keyPressList[i].direction === direction) {
-                        if (this.keyPressList[i].keyPressType == KeyPressEnum.Up) {
-                            this.keyPressList.push(new KeyPressDetails(direction, KeyPressEnum.Down));
+                        if (this.keyPressList[i].keyPressType === KeyPressEnum.Up) {
+                            // Previous key entry was an 'up' so we will add a new entry to the end of the list so it is ordered correctly
+                            this.keyPressList.push(new KeyPressDetails(direction));
                         }
 
                         return;
@@ -55,7 +57,7 @@ gameApp.service("KeyPressHandler", function() {
                 }
             }
 
-            this.keyPressList.push(new KeyPressDetails(direction, KeyPressEnum.Down));
+            this.keyPressList.push(new KeyPressDetails(direction));
         },
 
         keyRelease: function(keyCode) {
@@ -71,10 +73,10 @@ gameApp.service("KeyPressHandler", function() {
 
             for (var i = this.keyPressList.length - 1; i >= 0; i--) {
                 if (this.keyPressList[i].direction === direction) {
-                    if (this.keyPressList[i].processed || i != 0) {
+                    if (this.keyPressList[i].keyPressType !== KeyPressEnum.DownUnprocessed || i != 0) {
                         this.keyPressList.splice(i, 1);
                     } else{
-                        this.keyPressList.push(new KeyPressDetails(direction, KeyPressEnum.Up));
+                        this.keyPressList[i].keyPressType = KeyPressEnum.Up;
                     }
 
                     break;
@@ -87,15 +89,15 @@ gameApp.service("KeyPressHandler", function() {
 
             if (this.keyPressList && this.keyPressList.length) {
                 direction = this.keyPressList[0].direction;
-                if (!this.keyPressList[0].processed) {
-                    this.keyPressList[0].processed = true;
-                    for (var i = 1; i < this.keyPressList.length; i++) {
-                        if (this.keyPressList[i].direction == direction && this.keyPressList[i].keyPressType == KeyPressEnum.Up) {
-                            this.keyPressList.splice(i, 1);
-                            this.keyPressList.splice(0, 1);
-                            break;
-                        }
-                    }
+
+                switch (this.keyPressList[0].keyPressType) {
+                    case KeyPressEnum.DownUnprocessed:
+                        this.keyPressList[0].keyPressType = KeyPressEnum.DownProcessed;
+                        break;
+
+                    case KeyPressEnum.Up:
+                        this.keyPressList.splice(0, 1);
+                        break;
                 }
             }
 
