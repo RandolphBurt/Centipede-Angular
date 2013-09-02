4f67ac0a646b419b7358cb4458dc5c6020b9fe19
randolphburt <randolph.burt@gmail.com>
2013-09-02 21:19:29 +0100

    Change GlobalSettings to be a 'Constant' rather than a 'Factory' service.

--- App/js/services/GlobalSettings.js
+++ App/js/services/GlobalSettings.js
@@ -1,29 +1,27 @@
 'use strict';
 
-gameApp.factory("GlobalSettings", function() {
-   return {
-       spriteSize: 20,
-       spriteSheetWidth: 4,
-       playerAreaHeight: 6,
+gameApp.constant("GlobalSettings", {
+   spriteSize: 20,
+   spriteSheetWidth: 4,
+   playerAreaHeight: 6,
 
-       // board creation
-       mushroomChanceNonPlayerArea: 10,
-       mushroomChancePlayerArea: 40,
+   // board creation
+   mushroomChanceNonPlayerArea: 10,
+   mushroomChancePlayerArea: 40,
 
-       minMushroomsBeforeFleaCreated: 40,
-       minMushroomsInPlayerAreaBeforeFleaCreated: 5,
+   minMushroomsBeforeFleaCreated: 40,
+   minMushroomsInPlayerAreaBeforeFleaCreated: 5,
 
-       fleaCreationChance: 100,
-       spiderCreationChance: 25,
-       snailCreationChance: 200,
+   fleaCreationChance: 100,
+   spiderCreationChance: 25,
+   snailCreationChance: 200,
 
-       maxBulletsOnScreen: 5,
+   maxBulletsOnScreen: 5,
 
-       scoreHitMushroom: 10,
-       scoreHitPoisonMushroom: 25,
-       scoreHitCentipede: 100,
-       scoreHitSpider: 1000,
-       scoreHitFlea: 250,
-       scoreHitSnail: 500
-   }
+   scoreHitMushroom: 10,
+   scoreHitPoisonMushroom: 25,
+   scoreHitCentipede: 100,
+   scoreHitSpider: 1000,
+   scoreHitFlea: 250,
+   scoreHitSnail: 500
 });
