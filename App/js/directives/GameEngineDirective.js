232208d7513dc587cfe4771f286c139966d951d3
randolphburt <randolph.burt@gmail.com>
2014-01-04 17:27:42 +0000

    Update version of Angular to 1.2.7 and thus convert nested timeout calls to interval.

--- App/js/directives/GameEngineDirective.js
+++ App/js/directives/GameEngineDirective.js
@@ -1,21 +1,18 @@
 'use strict';
 
-gameApp.directive('gameEngine', function($timeout, GameEngine) {
+gameApp.directive('gameEngine', function($interval, GameEngine) {
     return {
         restrict: 'A',
         template: '<canvas id="gameCanvas" width="600" height="640" style="border:1px solid #000000;"></canvas>',
 
         link: function(scope, element, attrs, controller) {
-            var timer;
+            var intervalPromise;
             var animation = 0;
-            var date = new Date();
             var canvas = element.find('canvas')[0].getContext("2d");
 
             GameEngine.initialise(canvas, 'App/img/graphics.png');
 
             function gameLoop() {
-                var nextTick = date.getTime() + 50;
-
                 animation++;
 
                 if (animation == 4) {
@@ -23,15 +20,14 @@ gameApp.directive('gameEngine', function($timeout, GameEngine) {
                 }
 
                 GameEngine.update(animation);
+            };
 
-                timer = $timeout(gameLoop, Math.max(0, nextTick - date.getTime()));
-            }
-
-            gameLoop();
+            intervalPromise = $interval(gameLoop, Math.max(50));
 
             scope.$on("$destroy", function() {
-               if (timer) {
-                   $timeout.cancel(timer);
+               if (intervalPromise) {
+                   $interval.cancel(intervalPromise);
+                   intervalPromise = undefined;
                }
             });
         }
